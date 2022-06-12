987350d70d5e3aac4431c4985eb87c1bebe61af2
HarikaChalla03 <107341665+HarikaChalla03@users.noreply.github.com>
2022-06-12 13:39:59 +0530

    Add files via upload
    Uploading first basic java program into the repository.

---
+++ Basic_Java_Program.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+public class Basic_Java_Program {
+
+	public static void main(String[] args) {
+		//declare variables and assign values
+		int num1=10;
+		float num2=12;
+		char character='H';
+		boolean bool=true;
+		String str="Great Learning";
+		double num3=10.23;
+		//display all the variables on the console
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(character);		
+		System.out.println(bool);
+		System.out.println(str);
+		System.out.println(num3);
+	}
+
+}
